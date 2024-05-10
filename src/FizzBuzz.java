ce18675dfc3c985a3939b6d9174dfaad2955071c
suzukiyota <169060298+1001yyy1001@users.noreply.github.com>
2024-05-10 13:24:23 +0900

    課題提出

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,4 +1,19 @@
 
 public class FizzBuzz {
-
+	public static void main(String[] args) {
+	        for (int fz = 1; fz <= 100; fz++) {
+	            if (fz % 3 == 0 && fz % 5 == 0) {
+	                System.out.println("FizzBuzz");
+	            }
+	            if (fz % 3 == 0 && fz % 5 != 0) {
+	                System.out.println("Fizz");
+	            }
+	            if (fz % 3 != 0 && fz % 5 == 0) {
+	                System.out.println("Buzz");
+	            }
+	            if (fz % 3 != 0 && fz % 5 != 0) {
+	                System.out.println(fz);
+	            }
+	        }
+	    }
 }
